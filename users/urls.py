14c043b40b60a20014bdffe813f0061fe66a26dd
Alexander Morales-Panitz <alexander@moralespanitz.com>
2024-10-13 07:02:24 -0500

    feat: socios

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, get_user_info, get_users_from_junta
+from .views import UserViewSet, get_user_info, get_users_from_junta, get_member_by_dni, get_member_prestamos, get_member_pagos, get_member_multas, get_member_acciones
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,4 +9,9 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/info', get_user_info ),
     path('api/junta-users/<int:junta_id>/', get_users_from_junta),
+    path('api/members/dni/<str:dni>/', get_member_by_dni),
+    path('api/members/dni/<str:dni>/prestamos/', get_member_prestamos),
+    path('api/members/dni/<str:dni>/pagos/', get_member_pagos),
+    path('api/members/dni/<str:dni>/multas/', get_member_multas),
+    path('api/members/dni/<str:dni>/acciones/', get_member_acciones),
 ]
--- users/views.py
+++ users/views.py
@@ -3,11 +3,17 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions, viewsets
 from rest_framework.permissions import IsAdminUser
 from .serializer import UserSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from juntas.models import Junta
 User = get_user_model()
 from .permissions import IsAdminOrReadOnly, IsFacilitatorOrReadOnly, IsDirectorOrReadOnly, IsPartnerOrReadOnly
+from prestamos.serializers import PrestamoSerializer, PagosPrestamosSerializer
+from multas.serializers import MultaSerializer
+from prestamos.models import Prestamo, PagosPrestamos
+from acciones.serializers import AccionPurchaseSerializer, AccionPurchase
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -51,4 +57,59 @@ def get_user_info(request):
             'last_name': user.last_name,
         })
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_member_by_dni(request, dni):
+    try:
+        member = User.objects.filter(document_number=dni).first()
+        serializer = UserSerializer(member)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_member_prestamos(request, dni):
+    try:
+        user = User.objects.filter(document_number=dni).first()
+        prestamos = Prestamo.objects.filter(member=user)
+        serializer = PrestamoSerializer(prestamos, many=True)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_member_pagos(request, dni):
+    try:
+        user = User.objects.filter(document_number=dni).first()
+        prestamos = Prestamo.objects.filter(member=user)
+        pagos = PagosPrestamos.objects.filter(prestamo__in=prestamos)
+        serializer = PagosPrestamosSerializer(pagos, many=True)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_member_multas(request, dni):
+    member = User.objects.filter(document_number=dni).first()
+    multas = member.multas.all()
+    serializer = MultaSerializer(multas, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_member_acciones(request, dni):
+    try:
+        user = User.objects.filter(document_number=dni).first()
+        acciones = AccionPurchase.objects.filter(member=user)
+        serializer = AccionPurchaseSerializer(acciones, many=True)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
